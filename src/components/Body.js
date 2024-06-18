5938ab4d2b348658ac01312ad83343fc1aaf099c
Med008med <155750340+Med008med@users.noreply.github.com>
2024-06-18 16:26:36 +0200

    Add files via upload

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='bdy'>
+        <h1>about us</h1>
+        <p>first react.js app</p>
+      
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='navbar'>
+        <a href=''>Home page</a>
+        
+       <div className='nav'>
+        <a href=''>About</a>
+        <a href=''>Contacts</a>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
